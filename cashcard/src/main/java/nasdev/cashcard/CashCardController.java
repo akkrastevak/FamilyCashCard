36b47e30e5325cc6dbe4c01df4354348923df5c2
akkrastevak <atanas.krastev@abv.bg>
2023-06-09 13:40:03 +0300

    added couple GET methods enpoints, using pagination and added tests for those endpoints

--- cashcard/src/main/java/nasdev/cashcard/CashCardController.java
+++ cashcard/src/main/java/nasdev/cashcard/CashCardController.java
@@ -1,11 +1,16 @@
 package nasdev.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.Optional;
+import java.util.*;
+
 
 @RestController
 @RequestMapping("/cashcards")
@@ -36,5 +41,17 @@ public class CashCardController {
                 .buildAndExpand(savedCashCard.id())
                 .toUri();
         return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CashCard>> findAll(Pageable pageable){
+
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                ));
+        return ResponseEntity.ok(page.getContent());
     }
 }
