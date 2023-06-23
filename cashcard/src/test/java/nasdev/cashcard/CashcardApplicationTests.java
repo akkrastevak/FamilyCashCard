ff54c4c2e6b813515003dc99edf99911516479f4
akkrastevak <atanas.krastev@abv.bg>
2023-06-23 15:44:11 +0300

    added spring security dependency

--- cashcard/src/test/java/nasdev/cashcard/CashcardApplicationTests.java
+++ cashcard/src/test/java/nasdev/cashcard/CashcardApplicationTests.java
@@ -50,7 +50,7 @@ class CashcardApplicationTests {
     @Test
     @DirtiesContext
     void shouldCreateANewCashCard() {
-        CashCard newCashCard = new CashCard(null, 250.00);
+        CashCard newCashCard = new CashCard(null, 250.00, "sarah1");
         ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
 
         assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
